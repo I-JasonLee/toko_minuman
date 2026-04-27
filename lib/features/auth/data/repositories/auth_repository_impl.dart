55d9c2deee8c7604aeffd0155455c4e8235e38a7
Jason <1123150116@global.ac.id>
2026-04-28 01:46:05 +0700

    update auth_repository_impl.dart

--- lib/features/auth/data/repositories/auth_repository_impl.dart
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -1,15 +1,26 @@
-import '../../../../core/constants/api_constants.dart';
-import '../../../../core/services/dio_client.dart';
+// import '../../../../core/constants/api_constants.dart';
+// import '../../../../core/services/dio_client.dart';
+// import '../../domain/repositories/auth_repository.dart';
+
+// class AuthRepositoryImpl implements AuthRepository {
+//   @override
+//   Future<String> verifyFirebaseToken(String firebaseToken) async {
+//     final response = await DioClient.instance.post(
+//       ApiConstants.verifyToken,
+//       data: {'firebase_token': firebaseToken},
+//     );
+//     final data = response.data['data'] as Map<String, dynamic>;
+//     return data['access_token'] as String;
+//   }
+// }
+
 import '../../domain/repositories/auth_repository.dart';
 
 class AuthRepositoryImpl implements AuthRepository {
   @override
   Future<String> verifyFirebaseToken(String firebaseToken) async {
-    final response = await DioClient.instance.post(
-      ApiConstants.verifyToken,
-      data: {'firebase_token': firebaseToken},
-    );
-    final data = response.data['data'] as Map<String, dynamic>;
-    return data['access_token'] as String;
+    // Pura-pura loading 1 detik, lalu kembalikan token sukses!
+    await Future.delayed(const Duration(seconds: 1));
+    return "dummy_access_token_12345";
   }
 }
