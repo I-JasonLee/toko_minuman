bc1ec162843e3a43c712d902c480fd0be20f4589
Jason <1123150116@global.ac.id>
2026-07-01 23:24:34 +0700

    update

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,9 +1,5 @@
 class ApiConstants {
-<<<<<<< HEAD
   static const String baseUrl = 'http://192.168.0.28:8080/v1';
-=======
-  static const String baseUrl = 'http://10.0.2.2:8080/v1';
->>>>>>> 52b362492a0d4bb37457296bc8afd097bc83b256
 
   // Auth endpoints
   static const String verifyToken = '/auth/verify-token';
